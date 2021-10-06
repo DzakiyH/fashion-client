8d29956df7fd8a1183ce35d0f069ddc3fdb2b3c6
DzakiyH <dzakiy.h@mail.ugm.ac.id>
2021-10-06 20:20:14 +0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Views/home';
+import Product from './Views/product';
+import Shop from './Views/shop';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path='/' exact>
+            <Home />
+          </Route>
+          <Route path='/products'>
+            <Product />
+          </Route>
+          <Route path='shop'>
+            <Shop />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
